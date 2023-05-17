013a3b4a893e2512cfd1b9836b5b5185f3661936
novianmw <novian.mulyoadi.w@gmail.com>
2023-05-17 20:31:32 +0700

    delete junk items

--- components/BUG, TESTING, JUNK/NextArrow.tsx
+++
@@ -1,9 +0,0 @@
-import { RxArrowRight } from "react-icons/rx";
-
-export default function NextArrow({onClick}:any) {
-    return (
-        <button onClick={onClick} className="flex justify-center items-center py-4 px-6 border rounded-full hover:bg-[#FFA500] hover:text-black duration-300">
-            <RxArrowRight className="w-5 h-5"/>
-        </button>
-  )
-}
--- components/BUG, TESTING, JUNK/PrevArrow.tsx
+++
@@ -1,9 +0,0 @@
-import { RxArrowLeft } from "react-icons/rx";
-
-export default function PrevArrow({onClick}:any) {
-    return (
-            <button onClick={onClick} className="flex justify-center items-center py-4 px-6 border rounded-full hover:bg-[#FFA500] hover:text-black duration-300">
-                <RxArrowLeft className="w-5 h-5"/>
-            </button>
-    )
-}
