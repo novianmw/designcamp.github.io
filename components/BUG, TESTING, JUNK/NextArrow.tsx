e51955ef56aaaf75e228f078b8d4d31fb9838b84
novianmw <novian.mulyoadi.w@gmail.com>
2023-05-17 16:16:06 +0700

    update file

--- components/BUG, TESTING, JUNK/NextArrow.tsx
+++ components/BUG, TESTING, JUNK/NextArrow.tsx
@@ -1,6 +1,6 @@
 import { RxArrowRight } from "react-icons/rx";
 
-export default function NextArrow({onClick}) {
+export default function NextArrow({onClick}:any) {
     return (
         <button onClick={onClick} className="flex justify-center items-center py-4 px-6 border rounded-full hover:bg-[#FFA500] hover:text-black duration-300">
             <RxArrowRight className="w-5 h-5"/>
--- components/BUG, TESTING, JUNK/PrevArrow.tsx
+++ components/BUG, TESTING, JUNK/PrevArrow.tsx
@@ -1,6 +1,6 @@
 import { RxArrowLeft } from "react-icons/rx";
 
-export default function PrevArrow({onClick}) {
+export default function PrevArrow({onClick}:any) {
     return (
             <button onClick={onClick} className="flex justify-center items-center py-4 px-6 border rounded-full hover:bg-[#FFA500] hover:text-black duration-300">
                 <RxArrowLeft className="w-5 h-5"/>
