ac955ee4c56b789f54c0e1984139084e20eebb20
novianmw <novian.mulyoadi.w@gmail.com>
2023-05-17 20:55:59 +0700

    update copyright

--- components/Footnote.tsx
+++ components/Footnote.tsx
@@ -3,7 +3,7 @@ export default function Footnote() {
         <section className="flex flex-row justify-between items-center bg-[#1C2024] py-6 px-7 w-full">
             <div className="flex flex-row justify-start items-center text-white">
                 <h1 className={`font-normal text-[16px] ml-3`}>
-                    © 2023 CodePolitan. All rights reserved.
+                    © 2023 by Novian MW. All rights reserved.
                 </h1>
             </div>
             <div className="flex flex-row justify-end items-center gap-4 text-white">
