a38ccd63b9335ee407ac2c9a3283ba60adac927b
novianmw <novian.mulyoadi.w@gmail.com>
2023-05-23 20:11:34 +0700

    "update minor for responsive design"

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -7,17 +7,17 @@ const slab = Montagu_Slab({ subsets: ["latin"], display: "swap" })
 export default function  Faq () {
 
     return (
-        <section className="flex flex-row items-start w-full py-12 px-6">
-            <div className="flex flex-col items-start w-[400px]">
-                <div className="flex flex-col items-start py-12 gap-5">
-                    <h1 className={`font-semibold text-[36px] w-[350px] ${slab.className}`}>
+        <section className="flex flex-col md:flex-row items-start w-full p-12">
+            <div className="flex flex-col items-start w-full max-w-[400px] py-12">
+                <div className="flex flex-col justify-center items-center text-center md:text-left md:justify-start md:items-start py-12 gap-5">
+                    <h1 className={`font-semibold text-[36px] ${slab.className}`}>
                         <span className="underline underline-offset-1 decoration-8 decoration-[#FFA500]">Frequently Asked </span>Questions
                     </h1>
                     <p className="font-normal text-[16px] text-[#878787]">
                         Get answers to your questions about DesignCamp&apos;s graphic design course.
                     </p>
                 </div>
-                <div className="flex flex-col items-start w-[400px] gap-5">
+                <div className="hidden md:flex flex-col items-start w-[400px] gap-5">
                     <h1 className="font-semibold text-[16px]">
                         Have other questions?
                     </h1>
@@ -32,6 +32,14 @@ export default function  Faq () {
                         <ContentToggle content={question} {...question} />
                     </div>
                 ))}
+                <div className="flex md:hidden flex-col justify-center items-center w-full gap-5">
+                    <h1 className="font-semibold text-[16px]">
+                        Have other questions?
+                    </h1>
+                    <button className="flex justify-center items-center py-4 px-6 border rounded-full bg-black text-white hover:bg-[#FFA500] hover:text-black duration-300">
+                        Contact Us
+                    </button>
+                </div>
             </div>
         </section>
     )
